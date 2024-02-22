a0063ddfd8363776a7a280809f7a7e816f1184d9
jvcmtr <jvictorcmtr@gmail.com>
2024-02-21 22:49:46 -0300

    imported data from mithra data repo

--- mithra_app/App.js
+++ mithra_app/App.js
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://jvcmtr.github.io/mithra_data/data.json');
+      if (!response.ok) {
+        throw new Error('Erro no github');
+      }
+      const d = await response.json();
+      setData(d);
+    } catch (error) {
+      console.error('Erro:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        {data? JSON.stringify(data) : "loading"}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
